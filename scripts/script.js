e268fad203e31af37a4baeff0551d0be342e64b3
barbielm <barbosagabrie0508l@gmail.com>
2021-04-11 09:24:26 -0300

    virada das cartas

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 let nCards = 0
 while(nCards<4 || nCards>14 || nCards%2==1){
-    
+    alert("Digite um número par entre 4 e 14!")
     nCards = prompt("Com quantas cartas você deseja jogar?")
-    if(nCards<4 || nCards>14 || nCards%2==1){
-        alert("Digite um número par entre 4 e 14!")
-    }
+}
+let listOfCards = document.querySelector(".cards")
+
+for(let i=0; i<nCards; i++){
+    listOfCards.innerHTML += `<li class="card">
+                                <div class="front-face face">
+                                    <img src="imgs/front.png"/>
+                                </div>
+                                <div class="back-face face">
+                                    <img src="imgs/explodyparrot.gif"/>
+                                </div>
+                            </li>`
 }
